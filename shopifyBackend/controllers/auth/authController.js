46f90021ea06da6c6e9e5adaed56cb18706915fd
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-01-18 19:54:33 +0530

    Login

--- shopifyBackend/controllers/auth/authController.js
+++ shopifyBackend/controllers/auth/authController.js
@@ -1,7 +1,10 @@
 import vine, { errors } from "@vinejs/vine";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { registerSchema } from "../../validations/auth/authValidations.js";
+import {
+  loginSchema,
+  registerSchema,
+} from "../../validations/auth/authValidations.js";
 import prisma from "../../db/db.config.js";
 
 // Register user
@@ -50,11 +53,58 @@ export const register = async (req, res) => {
 
     return res.status(400).json({
       errors: {
-        email: "User doesn't exist!",
+        message: "Something went wrong while creating user!",
       },
     });
   } catch (error) {
-    console.log({ error: error });
+    if (error instanceof errors.E_VALIDATION_ERROR) {
+      return res.status(400).json({ errors: error.messages });
+    } else {
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong!",
+      });
+    }
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const body = req.body;
+
+    // Validate request body
+    const validator = vine.compile(loginSchema);
+    const payload = await validator.validate(body);
+
+    // Find user
+    const user = await prisma.users.findUnique({
+      where: { email: payload.email },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist!" });
+    } else {
+      // Decrypt password
+      if (!bcrypt.compareSync(payload.password, user.password)) {
+        return res.status(400).json({ message: "Invalid credentials!" });
+      }
+      const payloadData = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      };
+
+      // Issue token to login
+      const token = jwt.sign(payloadData, process.env.JWT_SECRET, {
+        expiresIn: "365d",
+      });
+
+      return res.status(200).json({
+        message: "Logged in successfully!",
+        access_token: `Bearer ${token}`,
+      });
+    }
+  } catch (error) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
       return res.status(400).json({ errors: error.messages });
     } else {
--- shopifyBackend/router/auth/authRouter.js
+++ shopifyBackend/router/auth/authRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { register } from "../../controllers/auth/authController.js";
+import { login, register } from "../../controllers/auth/authController.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
-// authRouter.post("/login", login);
+authRouter.post("/login", login);
 
 export default authRouter;
