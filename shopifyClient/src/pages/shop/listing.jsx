af134668d028aad405e8b99cc58a79cdd1743717
Rajesh <rajeshranjan8271@gmail.com>
2025-01-22 03:50:57 +0530

    Added sort options

--- shopifyClient/src/pages/shop/listing.jsx
+++ shopifyClient/src/pages/shop/listing.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import ShopFilter from "@/components/shop/filter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+import { sortOptions } from "@/config/config";
 
 const ShopListing = () => {
   return (
     <div className="gap-6 grid grid-cols-1 md:grid-cols-[300px_1fr] p-4 md:p-6">
       <ShopFilter />
+      <div className="bg-background shadow-sm rounded-lg w-full">
+        <div className="flex justify-between items-center p-4 border-b">
+          <h2 className="font-extrabold text-lg">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">10 Products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDown className="w-4 h-4" />
+                  <span>Sort By</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem key={sortItem.id}>
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
