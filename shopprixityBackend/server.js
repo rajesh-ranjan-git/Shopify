4119d4e510ea68d54fb77255f76be2aaa8fc4879
Rajesh <rajeshranjan8271@gmail.com>
2025-02-04 00:36:26 +0530

    Working on deployement

--- shopprixityBackend/server.js
+++ shopprixityBackend/server.js
@@ -39,10 +39,14 @@ server.use(express.urlencoded({ extended: false }));
 server.use("/api", router);
 
 // Server response
+// server.get("/", (req, res) => {
+//   return res.json({
+//     message: `Shoprixity server is running at http://localhost:${PORT}`,
+//   });
+// });
+
 server.get("/", (req, res) => {
-  return res.json({
-    message: `Shoprixity server is running at http://localhost:${PORT}`,
-  });
+  return res.send("Shoprixity server is running!");
 });
 
 // Listen to server
